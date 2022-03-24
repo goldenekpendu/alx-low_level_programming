da72b4db6d6609107228a9cb08f9316ba11cedf7
goldenekpendu <ekpendugolden1@gmail.com>
2022-03-24 01:36:52 +0000

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,15 +16,15 @@ void rev_string(char *s)
 	while (s[i] != '\0')
 	{
 		i++;
-		max = i - 1;
-		half = max / 2;
-		while (half >= 0)
-		{
-			first = s[max - half];
-			last = s[half];
-			s[half] = first;
-			s[max - half] = last;
-			half--;
-		}
+	}
+	max = i - 1;
+	half = max / 2;
+	while (half >= 0)
+	{
+		first = s[max - half];
+		last = s[half];
+		s[half] = first;
+		s[max - half] = last;
+		half--;
 	}
 }
